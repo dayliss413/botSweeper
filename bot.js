9a63011a89c28ecd21fbaf1cbe00ad2b6972cdaf
dayliss <h.g@smithsinla.com>
2013-11-24 11:06:03 -0800

    make gh-pages branch

---
+++ bot.js
@@ -0,0 +1,11 @@
+var game;
+var startGame = function(x,y,width,height,bombs) {
+  game = new Game(x,y,width,height,bombs);
+  printGame(game,true);
+  click(x,y);
+}
+
+var click = function(x,y) {
+  
+  
+}
