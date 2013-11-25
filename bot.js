cd76ca51c31441fb4838f0128e1c8c2fedcb8355
dayliss <h.g@smithsinla.com>
2013-11-25 15:42:52 -0800

    make expanding click

--- bot.js
+++ bot.js
@@ -1,10 +1,48 @@
 var game;
 var startGame = function(x,y,width,height,bombs) {
   game = new Game(x,y,width,height,bombs);
-  printGame(game,true);
-  click(x,y);
+  console.log("finished mapping");
+  return expandClick(x,y);
 }
 
-var click = function(x,y) {
-  
+var expandClick = function(x,y) {
+  var expandTo = new Array();
+  expandTo[0] = {}
+  expandTo[0].x = x;
+  expandTo[0].y = y;
+  while(expandTo.length > 0) {
+    game.click(expandTo[0].x,expandTo[0].y);
+	if(getSpace(expandTo[0].x,expandTo[0].y) == 0) {
+	  for(var i = expandTo[0].x-1; i <= 1+expandTo[0].x;i++) {
+	    for(var j = expandTo[0].y-1; j <= 1+expandTo[0].y;j++) {
+	      if(getSpace(i,j) == 'empty') {
+		    expandTo.push({});
+			expandTo[expandTo.length-1].x = i;
+			expandTo[expandTo.length-1].y = j;
+			game.click(i,j);
+		  }
+	    }
+	  }
+	}
+	expandTo.splice(0,1);
+  }
+  console.log("finished expanding");
+  printGame(game,true);
+  console.log("finished printing");
 }
+
+var getSpace = function(x,y) {
+  if(game.getRevealed()[x] != undefined && game.getRevealed()[x][y] != undefined) {
+    if(game.getRevealed()[x][y] === ' ') {  //in javascript, empty strings are equal to 0. this will make it more clear
+      return 'empty';
+	}
+	else {
+	  return game.getRevealed()[x][y];
+	}
+  }
+  else {
+    return 'outOfBounds';
+  }
+}
+
+//startGame(10,10,20,20,10);
